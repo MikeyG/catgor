a6d5c9c21179eda320622862ac8455aba3ab274a
mgreene <mikeos2@gmail.com>
2015-01-13 05:55:38 -0500

    Playing with window

--- catgor/CatgorApplication.py
+++ catgor/CatgorApplication.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('catgor')
 
 class MyWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
-        Gtk.Window.__init__(self, title="Menubutton Example", application=app)
+        Gtk.Window.__init__(self, title="Catgor", application=app)
         self.set_default_size(600, 400)
 		
         grid = Gtk.Grid()
@@ -21,9 +21,10 @@ class MyWindow(Gtk.ApplicationWindow):
 		
         menumodel = Gio.Menu()
         menubutton.set_menu_model(menumodel)
-        menumodel.append("New", "app.new")
+        menumodel.append("Help", "app.help")
+        menumodel.append("About", "app.about")
         menumodel.append("Quit", "app.quit")
-		
+        		
         self.add(grid)
 
 class CatgorApplication(Gtk.Application):
@@ -38,17 +39,43 @@ class CatgorApplication(Gtk.Application):
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
-        new_action = Gio.SimpleAction.new("new", None)
-        new_action.connect("activate", self.new_callback)
-        self.add_action(new_action)
+#        new_action = Gio.SimpleAction.new("new", None)
+#        new_action.connect("activate", self.new_callback)
+#        self.add_action(new_action)
+
+#        quit_action = Gio.SimpleAction.new("quit", None)
+#        quit_action.connect("activate", self.quit_callback)
+#        self.add_action(quit_action)
+
+
+
+#        self.menu = Gio.Menu()
+#        self.menu.append(_("Help"), "app.help")
+#        self.menu.append(_("About"), "app.about")
+#        self.menu.append(_("Quit"), "app.quit")
+
+#        if session == 'gnome':
+#            # Configure GMenu
+#            self.set_app_menu(self.menu)
+
+        help_action = Gio.SimpleAction.new("help", None)
+        help_action.connect("activate", self.help_cb)
+        self.add_action(help_action)
+
+        about_action = Gio.SimpleAction.new("about", None)
+        about_action.connect("activate", self.about_cb)
+        self.add_action(about_action)
 
         quit_action = Gio.SimpleAction.new("quit", None)
-        quit_action.connect("activate", self.quit_callback)
+        quit_action.connect("activate", self.quit_cb)
         self.add_action(quit_action)
 
-    def new_callback(self, action, parameter):
-        print("You clicked \"New\"")
+    def help_cb(self, action, parameter):
+        print("You clicked \"Help\"")
 
-    def quit_callback(self, action, parameter):
+    def about_cb(self, action, parameter):
+        print("You clicked \"About\"")
+
+    def quit_cb(self, action, parameter):
         print("You clicked \"Quit\"")
         self.quit()
