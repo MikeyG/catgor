e8574027b3913fc2e264cd1efe5f0024cb445346
mgreene <mikeos2@gmail.com>
2015-01-15 06:48:42 -0500

    Finish about dialog

--- catgor/CatgorApplication.py
+++ catgor/CatgorApplication.py
@@ -7,11 +7,12 @@ import logging
 logger = logging.getLogger('catgor')
 #        logger.debug("Catgor Application")
 
+from catgorconst import APP_STORE, VER_MAJOR, VER_MINOR
 
-class MyWindow(Gtk.ApplicationWindow):
+class CatgorWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.Window.__init__(self, title="Catgor", application=app)
-        self.set_default_size(600, 400)
+        self.set_default_size(900, 600)
 		
         grid = Gtk.Grid()
 		
@@ -21,10 +22,10 @@ class MyWindow(Gtk.ApplicationWindow):
 		
         menumodel = Gio.Menu()
         menubutton.set_menu_model(menumodel)
-        menumodel.append("Help", "app.help")
+        menumodel.append("New", "app.new")
         menumodel.append("About", "app.about")
         menumodel.append("Quit", "app.quit")
-        		
+		
         self.add(grid)
 
 class CatgorApplication(Gtk.Application):
@@ -33,49 +34,59 @@ class CatgorApplication(Gtk.Application):
         logger.debug("Catgor Application")
 
     def do_activate(self):
-        win = MyWindow(self)
+        win = CatgorWindow(self)
         win.show_all()
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
-#        new_action = Gio.SimpleAction.new("new", None)
-#        new_action.connect("activate", self.new_callback)
-#        self.add_action(new_action)
-
-#        quit_action = Gio.SimpleAction.new("quit", None)
-#        quit_action.connect("activate", self.quit_callback)
-#        self.add_action(quit_action)
-
-
-
-#        self.menu = Gio.Menu()
-#        self.menu.append(_("Help"), "app.help")
-#        self.menu.append(_("About"), "app.about")
-#        self.menu.append(_("Quit"), "app.quit")
-
-#        if session == 'gnome':
-#            # Configure GMenu
-#            self.set_app_menu(self.menu)
-
-        help_action = Gio.SimpleAction.new("help", None)
-        help_action.connect("activate", self.help_cb)
-        self.add_action(help_action)
+        new_action = Gio.SimpleAction.new("new", None)
+        new_action.connect("activate", self.new_callback)
+        self.add_action(new_action)
 
         about_action = Gio.SimpleAction.new("about", None)
         about_action.connect("activate", self.about_cb)
         self.add_action(about_action)
-
+ 
         quit_action = Gio.SimpleAction.new("quit", None)
         quit_action.connect("activate", self.quit_cb)
         self.add_action(quit_action)
 
-    def help_cb(self, action, parameter):
-        print("You clicked \"Help\"")
+    def new_callback(self, action, parameter):
+        print("You clicked \"New\"")
 
-    def about_cb(self, action, parameter):
-        print("You clicked \"About\"")
+    def about_cb(self, widget, data=None):
+        """About callback function.  Display the AboutDialog."""
+        # Create and display the AboutDialog.
+        aboutdialog = Gtk.AboutDialog()
 
-    def quit_cb(self, action, parameter):
-        print("You clicked \"Quit\"")
+        # Credits
+        authors = ["Michael Greene"]
+        documenters = ["Michael Greene"]
+
+        # Populate the AboutDialog with all the relevant details.
+        aboutdialog.set_title("About Catgor")
+        aboutdialog.set_program_name("Catgor")
+        aboutdialog.set_version(("%s.%s" % (VER_MAJOR, VER_MINOR)))        
+        aboutdialog.set_comments("Gnome overview editor")
+#        aboutdialog.set_logo_icon_name("catgor")
+        aboutdialog.set_copyright("Copyright \xc2\xa9 2014-2015 Michael Greene")
+        aboutdialog.set_authors(authors)
+        aboutdialog.set_documenters(documenters)
+        aboutdialog.set_website("https://github.com/MikeyG/catgor")
+        aboutdialog.set_website_label("Catgor Developer Website")
+
+        # Connect the signal to destroy the AboutDialog when Close is clicked.
+        aboutdialog.connect("response", self.about_close_cb)
+#        aboutdialog.set_transient_for(self.win)
+
+        # Show the AboutDialog.
+        aboutdialog.show()
+
+    def about_close_cb(self, widget, response):
+        """Destroy the AboutDialog when it is closed."""
+        widget.destroy()
+
+    def quit_cb(self, widget, data=None):
+        """Signal handler for closing the CatgorWindow."""
         self.quit()
